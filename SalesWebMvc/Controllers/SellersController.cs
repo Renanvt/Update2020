7e66970c94a0977c25a89037d41e423666b4e44a
Renanvt <jonatan_rvs@hotmail.com>
2020-10-12 14:43:44 -0300

    SellerService and Basic FindAll

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
+        //Dependencia para o SellerService
+        private readonly SellerService _sellerService;
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
         public IActionResult Index()
         {
-            return View();
+            //Retorna uma lista de Seller
+            var list = _sellerService.FindAll();
+            return View(list);
         }
     }
 }
---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,26 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        /*Dependencia para o dbcontext*/
+        private readonly SalesWebMvcContext _context;
+        //previne que a dependência não possa ser alterada
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+        //Retorna uma lista com todos os vendedores do banco de dados
+        public List<Seller> FindAll()
+        {
+            //Operacao sincrona, A aplicação vai ficar bloqueada, esperando essa ação terminar
+            return _context.Seller.ToList();
+        }
+    }
+}
